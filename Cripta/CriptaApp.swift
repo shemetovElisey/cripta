6a583ec77032033248b0d0c5b632cfe73754f1b0
Elisey Shemetov <eshemetov@corp.local>
2021-10-04 20:56:20 +0700

    Initial Commit

---
+++ Cripta/CriptaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CriptaApp.swift
+//  Cripta
+//
+//  Created by Elisey Shemetov on 04.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CriptaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
